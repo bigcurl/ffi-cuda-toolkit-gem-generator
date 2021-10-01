83db8b3396a35c00b00763bce15add7cfdc8b5a9
Samuel Goebert <sam@bigcurl.de>
2021-10-01 14:32:54 +0200

    added transformation from c to ffi types for struct

--- app/subcommands/generate/generate_driver_api.rb
+++ app/subcommands/generate/generate_driver_api.rb
@@ -35,7 +35,7 @@ class GenerateDriverApi < ApplicationSubcommand
     c_type_structs = parse_struct_html_pages(struct_html_pages_paths)
 
     ### Transform parsed output to ruby code
-    ruby_type_structs = transform_c_type_structs_into_ruby_type_structs(c_type_structs)
+    ruby_type_structs = transform_c_types_into_ffi_types(c_type_structs)
 
     ### Generate class from template
     # class InputEvent < FFI::Struct
@@ -100,6 +100,15 @@ class GenerateDriverApi < ApplicationSubcommand
         return_type = 'void *' if return_type == '*'
         return_type = 'void *' if return_type.include? '[MAX_PLANES]'
 
+        # TODO: Still problems with:
+        # CUDA_EXTERNAL_MEMORY_HANDLE_DESC_v1::@12::@13
+        # CUDA_EXTERNAL_SEMAPHORE_HANDLE_DESC_v1::@14::@15
+        # CUDA_EXTERNAL_SEMAPHORE_SIGNAL_PARAMS_v1::@16::@17
+        # CUDA_EXTERNAL_SEMAPHORE_SIGNAL_PARAMS_v1::@16::@19
+        # CUDA_EXTERNAL_SEMAPHORE_WAIT_PARAMS_v1::@20::@21
+        # CUDA_EXTERNAL_SEMAPHORE_WAIT_PARAMS_v1::@20::@23
+        # CUDA_EXTERNAL_SEMAPHORE_WAIT_PARAMS_v1::@20::@22
+
         layout << { function_name: function_name, return_type: return_type }
       end
 
@@ -108,7 +117,70 @@ class GenerateDriverApi < ApplicationSubcommand
     c_type_structs
   end
 
-  def transform_c_type_structs_into_ruby_type_structs(_c_type_structs)
+  def transform_c_types_into_ffi_types(c_type_structs)
     ruby_type_structs = []
+
+    c_type_structs.each do |c_type_struct|
+      struct_name = c_type_struct[:struct_name]
+      layout = []
+
+      layout = c_type_struct[:layout].map do |layout_pair|
+        puts layout_pair[:return_type] if ffi_types(layout_pair[:return_type]).nil?
+        layout_pair[:return_type] = ffi_types(layout_pair[:return_type])
+        layout_pair
+      end
+
+      ruby_type_structs << { struct_name: struct_name, layout: layout } unless layout.empty?
+    end
+    ruby_type_structs
+  end
+
+  def ffi_types(ffi_type)
+    {
+      'int' => :int,
+      'float' => :float,
+      'double' => :double,
+      'unsigned short' => :ushort,
+      'unsigned char' => :uchar,
+      'unsigned int' => :uint,
+      'unsigned long long' => :ulong_long,
+      'size_t' => :size_t,
+      'void *' => :pointer,
+      'const void *' => :pointer,
+      'CUarray_format' => :pointer,
+      'CUDA_ARRAY3D_DESCRIPTOR' => :pointer,
+      'CUexternalSemaphoreHandleType' => :pointer,
+      'CUexternalSemaphore*' => :pointer,
+      'CUDA_EXTERNAL_SEMAPHORE_SIGNAL_PARAMS' => :pointer,
+      'CUDA_EXTERNAL_SEMAPHORE_WAIT_PARAMS' => :pointer,
+      'CUhostFn' => :pointer,
+      'CUfunction' => :pointer,
+      'CUstream' => :pointer,
+      'CUarray' => :pointer,
+      'CUdeviceptr' => :pointer,
+      'CUmemorytype' => :pointer,
+      'CUcontext' => :pointer,
+      'CUmemAccessDesc' => :pointer,
+      'CUmemPoolProps' => :pointer,
+      'CUmipmappedArray' => :pointer,
+      'CUresourcetype' => :pointer,
+      'CUresourceViewFormat' => :pointer,
+      'CUaddress_mode' => :pointer,
+      'CUfilter_mode' => :pointer,
+      'CUaccessProperty' => :pointer,
+      'CUmemHandleType' => :pointer,
+      'CUmemOperationType' => :pointer,
+      'CUarraySparseSubresourceType' => :pointer,
+      'CUeglColorFormat' => :pointer,
+      'CUeglFrameType' => :pointer,
+      'CUmemAccess_flags' => :pointer,
+      'CUmemLocation' => :pointer,
+      'CUmemAllocationHandleType' => :pointer,
+      'CUmemAllocationType' => :pointer,
+      'CUmemLocationType' => :pointer,
+      'CUmemAllocationType' => :pointer,
+      'CUmemAllocationHandleType' => :pointer,
+      'CUexternalMemoryHandleType' => :pointer
+    }[ffi_type]
   end
 end
