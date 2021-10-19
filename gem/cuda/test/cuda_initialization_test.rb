30df2812a3fd60efa06d5be15da820477ea17cde
Samuel Goebert <sam@bigcurl.de>
2021-10-19 14:04:16 +0200

    Added more CudaInitializationTest

--- gem/cuda/test/cuda_initialization_test.rb
+++ gem/cuda/test/cuda_initialization_test.rb
@@ -6,4 +6,8 @@ class CudaInitializationTest < Minitest::Test
   def test_cu_init
     assert_equal(:CUDA_SUCCESS, Cuda::DriverApi.cuInit(0))
   end
+
+  def test_cu_init_shall_be_called_with_flag_zero
+    assert_equal(:CUDA_ERROR_INVALID_VALUE, Cuda::DriverApi.cuInit(1))
+  end
 end
