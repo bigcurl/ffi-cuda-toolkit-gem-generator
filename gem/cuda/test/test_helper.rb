d52f0036e2e4ffe7a3ed5a57a0ca9f4fc7b171d9
Samuel Goebert <sam@bigcurl.de>
2021-11-03 13:19:20 +0100

    require right gem

--- gem/cuda/test/test_helper.rb
+++ gem/cuda/test/test_helper.rb
@@ -3,5 +3,5 @@
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'cuda'
 
-require 'minitest/debugger' if ENV['DEBUG'] # run like "DEBUG=1 rake test"
+require 'minitest/byebug' if ENV['DEBUG'] # run like "DEBUG=1 rake test"
 require 'minitest/autorun'
