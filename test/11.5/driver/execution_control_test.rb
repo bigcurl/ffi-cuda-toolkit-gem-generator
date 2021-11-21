819563baad234c7c7d7d9e2928e89fdb0ce00a6b
Sam <sam@bigcurl.de>
2021-11-21 09:52:01 +0100

    Removed functions which are not needed

--- test/11.5/driver/execution_control_test.rb
+++ test/11.5/driver/execution_control_test.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 # Needed
+# CUresult cuLaunchHostFunc ( CUstream hStream, CUhostFn fn, void* userData )
+# CUresult cuLaunchKernel ( CUfunction f, unsigned int  gridDimX, unsigned int  gridDimY, unsigned int  gridDimZ, unsigned int  blockDimX, unsigned int  blockDimY, unsigned int  blockDimZ, unsigned int  sharedMemBytes, CUstream hStream, void** kernelParams, void** extra )
+# CUresult cuLaunchCooperativeKernel ( CUfunction f, unsigned int  gridDimX, unsigned int  gridDimY, unsigned int  gridDimZ, unsigned int  blockDimX, unsigned int  blockDimY, unsigned int  blockDimZ, unsigned int  sharedMemBytes, CUstream hStream, void** kernelParams )
+
+# Not Needed
 # CUresult cuFuncGetAttribute ( int* pi, CUfunction_attribute attrib, CUfunction hfunc )
 # CUresult cuFuncGetModule ( CUmodule* hmod, CUfunction hfunc )
 # CUresult cuFuncSetAttribute ( CUfunction hfunc, CUfunction_attribute attrib, int  value )
 # CUresult cuFuncSetCacheConfig ( CUfunction hfunc, CUfunc_cache config )
 # CUresult cuFuncSetSharedMemConfig ( CUfunction hfunc, CUsharedconfig config )
-# CUresult cuLaunchCooperativeKernel ( CUfunction f, unsigned int  gridDimX, unsigned int  gridDimY, unsigned int  gridDimZ, unsigned int  blockDimX, unsigned int  blockDimY, unsigned int  blockDimZ, unsigned int  sharedMemBytes, CUstream hStream, void** kernelParams )
 # CUresult cuLaunchCooperativeKernelMultiDevice ( CUDA_LAUNCH_PARAMS* launchParamsList, unsigned int  numDevices, unsigned int  flags )
-# CUresult cuLaunchHostFunc ( CUstream hStream, CUhostFn fn, void* userData )
-# CUresult cuLaunchKernel ( CUfunction f, unsigned int  gridDimX, unsigned int  gridDimY, unsigned int  gridDimZ, unsigned int  blockDimX, unsigned int  blockDimY, unsigned int  blockDimZ, unsigned int  sharedMemBytes, CUstream hStream, void** kernelParams, void** extra )
